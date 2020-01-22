bca17d042f168f1d20b8838478c2c98606f146c3
Juan <jmoralespuello@gmail.com>
2020-01-22 11:54:39 -0500

    Listas y sus métodos

--- listas_metodos.py
+++ listas_metodos.py
@@ -25,4 +25,36 @@ print(lista)
 lista.insert(2,"Nuevo")
 print(lista)
 
-# Método
+# Método extend: combina dos lista
+
+print(lista)
+iterable = "Cadena"
+lista.extend(iterable)
+print(lista)
+
+print(lista)
+iterable = (1,2,3,4)
+lista.extend(iterable)
+print(lista)
+
+# Méotodo pop: Extrae un elemento y lo borra de la lista
+
+print(lista)
+print(lista.pop()) # Si no se coloca indice, borra el último
+print(lista)
+
+print(lista)
+print(lista.pop(2))
+print(lista)
+
+# Método Remove
+
+print(lista)
+print(lista.remove(3))  # Elimina la primera ocurrencia
+print(lista)
+
+# Método Reverse
+
+print(lista)
+print(lista.reverse()) # Invierte la lista
+print(lista)
